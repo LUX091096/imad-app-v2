cbe70789c0c3916db964f4aec62f5f8a6bb3d166
LUX091096 <lakshmithamohan@yahoo.com>
2017-02-10 19:45:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/lux1', function (req, res) {
-  res.sendFile(path.join(__dirname,  'lux1.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname,  'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
