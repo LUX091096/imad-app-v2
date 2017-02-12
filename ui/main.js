908fc71e9b43013074d3eafef6b871979bf78af8
LUX091096 <lakshmithamohan@yahoo.com>
2017-02-12 12:38:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,18 @@
-console.log('Loaded!');
-
-var img = document.getElementById('madi')
-img.onclick = function()
-{
-img.style.marginLeft = '100px';
-};
+var button=document.getElementById("counter");
+button.onclick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readystate===XMLHttpRequest.DONE){
+            if(request.status==200)
+            {
+                var counter=request.responseText;
+                var span=document.getElementbyId('count');
+                span.innerHTML=counter.tostring();
+            }
+            
+            
+            
+        }
+        
+    }
+}
